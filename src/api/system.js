d5324b46fbc0daaeeae2da2db1f2a8c25cbe414b
xuexianlei <xuexianlei@bwton.com>
2018-11-09 20:45:57 +0800

    权限完成

---
+++ src/api/system.js
@@ -0,0 +1,15 @@
+import service from "../utils/request";
+
+// -----------日志---------------
+
+/**
+ * 分页获取登录日志列表
+ * @param params
+ */
+export function listAdminByPage(params) {
+    return service({
+        url: "/api/log/listLoginLogByPage",
+        method: 'POST',
+        data: params
+    })
+}
